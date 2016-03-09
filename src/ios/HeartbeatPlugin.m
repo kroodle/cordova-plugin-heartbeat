4aadbdcbaf893878536ef51148f595da13c69a43
Koen Romers <info@koenromers.com>
2016-03-09 14:12:15 +0100

    feat(ios): methods and listener

--- src/ios/HeartbeatPlugin.m
+++ src/ios/HeartbeatPlugin.m
@@ -1,15 +1,40 @@
 #import "HeartbeatPlugin.h"
 
+// @interface HeartbeatPlugin()<HeartBeatDelegate>
+//
+// @end
+
 @implementation HeartbeatPlugin
 
+- (void)pluginInitialize {
+  NSLog(@"Initializing HeartbeatPlugin");
+  // [[HeartbeatLib shared] new];
+  // [[HeartbeatLib shared] setDelegate:self];
+
+  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    [self onHeartBeat:75];
+  });
+}
+
 - (void)start:(CDVInvokedUrlCommand*)command {
-  // CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-  // [[self commandDelegate] sendPluginResult:pluginResult callbackId:[command callbackId]];
+  NSLog(@"Heartbeat start");
+  // [[HeartbeatLib shared] start]];
+  CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+  [[self commandDelegate] sendPluginResult:pluginResult callbackId:[command callbackId]];
 }
 
 - (void)stop:(CDVInvokedUrlCommand*)command {
-  // CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-  // [[self commandDelegate] sendPluginResult:pluginResult callbackId:[command callbackId]];
+  NSLog(@"Heartbeat stop");
+  // [[HeartbeatLib shared] stop]];
+  CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+  [[self commandDelegate] sendPluginResult:pluginResult callbackId:[command callbackId]];
+}
+
+- (void)onHeartBeat:(int)bpm {
+  NSLog(@"Heartbeat onHeartBeat");
+  CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt:bpm];
+  // [pluginResult setKeepCallbackAsBool:YES];
+  [[self commandDelegate] sendPluginResult:pluginResult callbackId:[command callbackId]];
 }
 
 
