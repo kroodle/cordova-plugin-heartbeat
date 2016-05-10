325dac6d114ae9c1c74a7fbe8657393dca15991e
Koen Romers <info@koenromers.com>
2016-05-10 08:54:22 +0200

    refactor(plugin): performance increase
    - run processes in the background

--- src/ios/HeartbeatPlugin.m
+++ src/ios/HeartbeatPlugin.m
@@ -16,62 +16,71 @@
 #define kPluginVersion @"1.0.0"
 
 - (void)pluginInitialize {
-
   [self setResultQueue:[[NSMutableArray alloc] init]];
   [self setLib:[[HeartbeatLib alloc] init]];
   [[self lib] setDelegate:self];
+  // Only show last 100 points for now to draw graph
+  // Might be something to make configurable in future
+  [[self lib] setPointsForGraph:500];
 }
 
 - (void)start:(CDVInvokedUrlCommand*)command {
-
-  [[self lib] start];
-
-  [self setMainCallbackId:[command callbackId]];
-  CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-  [pluginResult setKeepCallback:[NSNumber numberWithBool:YES]];
-  [[self commandDelegate] sendPluginResult:pluginResult callbackId:[command callbackId]];
+  [[self commandDelegate] runInBackground:^{
+    [[self lib] start];
+    [self setMainCallbackId:[command callbackId]];
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [pluginResult setKeepCallback:[NSNumber numberWithBool:YES]];
+    [[self commandDelegate] sendPluginResult:pluginResult callbackId:[command callbackId]];
+  }];
 }
 
 - (void)stop:(CDVInvokedUrlCommand*)command {
-
-  [[self lib] stop];
-
-  CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-  [[self commandDelegate] sendPluginResult:pluginResult callbackId:[command callbackId]];
+  [[self commandDelegate] runInBackground:^{
+    [[self lib] stop];
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [[self commandDelegate] sendPluginResult:pluginResult callbackId:[command callbackId]];
+  }];
 }
 
 #pragma callback methods
 
 - (void)sendResultQueue {
-
-  for (CDVPluginResult *pluginResult in _resultQueue) {
-    [[self commandDelegate] sendPluginResult:pluginResult callbackId:_mainCallbackId];
-  }
-  [_resultQueue removeAllObjects];
+  [[self commandDelegate] runInBackground:^{
+    if (_mainCallbackId != nil) {
+      for (CDVPluginResult *pluginResult in _resultQueue) {
+        [[self commandDelegate] sendPluginResult:pluginResult callbackId:_mainCallbackId];
+      }
+      [_resultQueue removeAllObjects];
+    }
+  }];
 }
 
 - (void)sendErrorResultWithType:(NSString *)type andMessage:(NSString *)message {
-  if (message != nil) {
-    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:message];
-    [pluginResult setKeepCallback:[NSNumber numberWithBool:YES]];
-    if (_mainCallbackId != nil) {
-      [[self commandDelegate] sendPluginResult:pluginResult callbackId:_mainCallbackId];
-    } else {
-      [_resultQueue addObject:pluginResult];
+  [[self commandDelegate] runInBackground:^{
+    if (message != nil) {
+      CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:message];
+      [pluginResult setKeepCallback:[NSNumber numberWithBool:YES]];
+      if (_mainCallbackId != nil) {
+        [[self commandDelegate] sendPluginResult:pluginResult callbackId:_mainCallbackId];
+      } else {
+        [_resultQueue addObject:pluginResult];
+      }
     }
-  }
+  }];
 }
 
 - (void)sendSuccessResult:(NSDictionary *)payload {
-  if (payload != nil) {
-    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:payload];
-    [pluginResult setKeepCallback:[NSNumber numberWithBool:YES]];
-    if (_mainCallbackId != nil) {
-      [[self commandDelegate] sendPluginResult:pluginResult callbackId:_mainCallbackId];
-    } else {
-      [_resultQueue addObject:pluginResult];
+  [[self commandDelegate] runInBackground:^{
+    if (payload != nil) {
+      CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:payload];
+      [pluginResult setKeepCallback:[NSNumber numberWithBool:YES]];
+      if (_mainCallbackId != nil) {
+        [[self commandDelegate] sendPluginResult:pluginResult callbackId:_mainCallbackId];
+      } else {
+        [_resultQueue addObject:pluginResult];
+      }
     }
-  }
+  }];
 }
 
 - (void)sendSuccessResultWithType:(NSString *)type andDictionary:(NSDictionary *)data {
@@ -105,108 +114,96 @@
 #pragma HeartbeatLibDelegate
 
 - (void)onPercentageCompleted:(int)percentage {
-  dispatch_async(dispatch_get_main_queue(), ^{
-    [self sendSuccessResultWithType:@"progress" andNumber:[NSNumber numberWithInt:percentage]];
-  });
+  [self sendSuccessResultWithType:@"progress" andNumber:[NSNumber numberWithInt:percentage]];
+  [self sendResultQueue];
 }
 
 - (void)onStatusChange:(HeartBeatStatus)status {
-  dispatch_async(dispatch_get_main_queue(), ^{
-    NSString *statusString = @"";
-    switch (status) {
-      case STARTED:
-        statusString = @"STARTED";
-        break;
-      case NO_FINGER:
-        statusString = @"NO_FINGER";
-        break;
-      case FINGER_DETECTED:
-        statusString = @"FINGER_DETECTED";
-        break;
-      case CALIBRATING:
-        statusString = @"CALIBRATING";
-        break;
-      case MEASURING:
-        statusString = @"MEASURING";
-        break;
-      case COMPLETED:
-        statusString = @"COMPLETED";
-        break;
-      default:
-        statusString = @"ERROR";
-        break;
-    }
-    self.currentStatus = status;
-    [self sendSuccessResultWithType:@"status" andString:statusString];
-  });
+  NSString *statusString = @"";
+  switch (status) {
+    case STARTED:
+      statusString = @"STARTED";
+      break;
+    case NO_FINGER:
+      statusString = @"NO_FINGER";
+      break;
+    case FINGER_DETECTED:
+      statusString = @"FINGER_DETECTED";
+      break;
+    case CALIBRATING:
+      statusString = @"CALIBRATING";
+      break;
+    case MEASURING:
+      statusString = @"MEASURING";
+      break;
+    case COMPLETED:
+      statusString = @"COMPLETED";
+      break;
+    default:
+      statusString = @"ERROR";
+      break;
+  }
+  self.currentStatus = status;
+  [self sendSuccessResultWithType:@"status" andString:statusString];
+  [self sendResultQueue];
 }
 
 - (void)onWarning:(HeartBeatWarning)warning {
-  dispatch_async(dispatch_get_main_queue(), ^{
-    NSString * warningString = @"";
-    switch(warning) {
-      case SHAKING:
-        warningString = @"SHAKING";
-      break;
-      case LOW_QUALITY:
-        warningString = @"LOW_QUALITY";
-      break;
-      default:
-        warningString = @"";
-      break;
-    }
-    [self sendSuccessResultWithType:@"warning" andString:warningString];
-  });
+  NSString * warningString = @"";
+  switch(warning) {
+    case SHAKING:
+      warningString = @"SHAKING";
+    break;
+    case LOW_QUALITY:
+      warningString = @"LOW_QUALITY";
+    break;
+    default:
+      warningString = @"";
+    break;
+  }
+  [self sendSuccessResultWithType:@"warning" andString:warningString];
+  [self sendResultQueue];
 }
 
 - (void)onHeartBeat:(int)bpm {
-  dispatch_async(dispatch_get_main_queue(), ^{
-    [self sendSuccessResultWithType:@"bpm" andNumber:[NSNumber numberWithInt:bpm]];
-  });
+  [self sendSuccessResultWithType:@"bpm" andNumber:[NSNumber numberWithInt:bpm]];
+  [self sendResultQueue];
 }
 
 - (void)onError:(HeartBeatError)error {
-  dispatch_async(dispatch_get_main_queue(), ^{
-    NSString * errorString = @"";
-    switch (error) {
-      case NO_FINGER_DETECTED:
-        errorString = @"NO_FINGER_DETECTED";
-        break;
-      case CAMERA_HAS_NO_PERMISSION:
-        errorString = @"CAMERA_HAS_NO_PERMISSION";
-        break;
-      case FINGER_PRESSURE:
-        errorString = @"FINGER_PRESSURE";
-        break;
-      case BAD_FINGER_POSITION:
-        errorString = @"BAD_FINGER_POSITION";
-        break;
-      case TO_MUCH_MOVEMENT:
-        errorString = @"TOO_MUCH_MOVEMENT";
-        break;
-      default:
-        errorString = @"";
-        break;
-    }
-    [self sendErrorResultWithType:@"error" andMessage:errorString];
-  });
+  NSString * errorString = @"";
+  switch (error) {
+    case NO_FINGER_DETECTED:
+      errorString = @"NO_FINGER_DETECTED";
+      break;
+    case CAMERA_HAS_NO_PERMISSION:
+      errorString = @"CAMERA_HAS_NO_PERMISSION";
+      break;
+    case FINGER_PRESSURE:
+      errorString = @"FINGER_PRESSURE";
+      break;
+    case BAD_FINGER_POSITION:
+      errorString = @"BAD_FINGER_POSITION";
+      break;
+    case TO_MUCH_MOVEMENT:
+      errorString = @"TOO_MUCH_MOVEMENT";
+      break;
+    default:
+      errorString = @"";
+      break;
+  }
+  [self sendErrorResultWithType:@"error" andMessage:errorString];
+  [self sendResultQueue];
 }
 
 - (void)onGraphArrayUpdated:(NSArray *)points {
-  dispatch_async(dispatch_get_main_queue(), ^{
-    NSMutableArray *array = [[NSMutableArray alloc] init];
-    for(float i = 0; i < points.count;i++) {
-      Pulse *pulse = (Pulse *)[points objectAtIndex:i];
-      [array addObject:@(pulse.value)];
-    }
-    [self sendSuccessResultWithType:@"graph" andArray:[NSArray arrayWithArray:array]];
-  });
+  [self sendSuccessResultWithType:@"graph" andArray:[NSArray arrayWithArray:points]];
+  [self sendResultQueue];
 }
 
 - (void)onHRVReady:(HRV *)hrv {
-  dispatch_async(dispatch_get_main_queue(), ^{
-    [self sendSuccessResultWithType:@"hrv" andDictionary:@{@"avnn": @(hrv.AVNN), @"bpm": [NSNumber numberWithInt:hrv.bpm]}];
-  });
+  [self sendSuccessResultWithType:@"hrv" andDictionary:@{@"avnn": @(hrv.AVNN), @"bpm": [NSNumber numberWithInt:hrv.bpm]}];
+  [self sendResultQueue];
 }
 
 @end
