ab027815a10d0f2348155a5ae110f4bb658bc489
Koen Romers <info@koenromers.com>
2016-09-26 15:33:18 +0200

    fix(privacy): camera permissions ios10

--- src/ios/HeartbeatPlugin.m
+++ src/ios/HeartbeatPlugin.m
@@ -13,7 +13,7 @@
 
 @implementation HeartbeatPlugin
 
-#define kPluginVersion @"1.0.9"
+#define kPluginVersion @"1.0.10"
 
 - (void)pluginInitialize {
   [self setResultQueue:[[NSMutableArray alloc] init]];
